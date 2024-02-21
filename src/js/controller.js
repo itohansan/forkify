f91371cd8c34a721408259234ccdf3c0df5149d3
itohansan <itohanidubor@gmail.com>
2024-02-21 09:36:10 +0100

    updating recipe servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,8 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+    //0) Update result view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
 
     //1) LOADING RECIPE
     await model.loadRecipe(id);
@@ -66,8 +68,19 @@ const controlPagination = function (goToPage) {
   //Render NEW pagination buttons
   paginationView.render(model.state.search);
 };
+
+const controlServings = function (newServings) {
+  //Update the recipe servings(in state)
+  model.updateServings(newServings);
+
+  //update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
